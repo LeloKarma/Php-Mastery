9a954acb0533504e8354980afb3a7c7e548b9e32
LeloKarma <Sharonlelo6@gmail.com>
2024-10-02 18:36:54 +0100

    feat:strings

--- index.php
+++ index.php
@@ -50,5 +50,40 @@ var_dump($a === $b); //false //identical operator
 var_dump($a != $b); //false
 var_dump($a !== $b); //true
 var_dump($a <> $c); //true
+echo '<br>';
+echo '<br>';
 
+/*
+*strings
+*/
+
+$text = ' How can a calm cram in a clean cream can?';
+echo strlen($text); //to get the number of chars in a string 42
+echo '<br>';
+
+$text = trim($text); //removes all blank spaces in a string
+echo $text;
+echo '<br>';
+
+echo strtoupper($text); //sends all chars to uppercases
+echo '<br>';
+
+echo strtolower($text); //sends all chars to lowercases
+echo '<br>';
+
+echo str_replace('can', 'could', $text); //replace all occurrence of 1st string with the second
+echo $text;
+echo '<br>';
+
+echo substr($text, 2, 6); // w coul extracts all chars in the given position
+
+var_dump(strpos($text, 'can')); // false if string couldnt be found
+var_dump(strpos($text, 'could')); // 4 shows the position of the 1st occurence of the given string
+echo '<br>';
+echo '<br>';
+
+$firstname = 'Lelo';
+$surname = 'Karma';
+
+echo 'I am '. $firstname . ' ' . $surname . '!.';
 ?>
